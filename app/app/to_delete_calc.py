c762eefd841eeede1cc3fc32a87faead284d57cd
igor.suchilov <igor.suchilov@gmail.com>
2020-11-01 14:45:49 +0000

    Added custom User model

---
+++ app/app/to_delete_calc.py
@@ -0,0 +1,10 @@
+
+
+def addcalc(x, y):
+    """Add two numbers together"""
+    return x + y
+
+
+def subcalc(x, y):
+    """Subtract x from y"""
+    return x - y
---
+++ app/app/to_delete_tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from app.calc import addcalc, subcalc
+
+
+class CalcTests(TestCase):
+
+
+    def test_add_numbers(self):
+        """Test that 2 numbers are added together"""
+        self.assertEqual(addcalc(3,8), 11)
+
+
+    def test_subtract_numbers(self):
+        """Test that 2 values are subtracted and returned"""
+        self.assertEqual(subcalc(10,5), 5)
